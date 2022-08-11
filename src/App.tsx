92b6b8a1d03e080eb75e011850bfd029da417df0
mrkvon <mrkvon@protonmail.com>
2022-08-11 21:54:44 +0300

    Refactor Map outside of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,15 @@
-import MarkerClusterGroup from '@changey/react-leaflet-markercluster'
-import * as L from 'leaflet'
 import 'leaflet.markercluster/dist/MarkerCluster.css'
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
 import 'leaflet/dist/leaflet.css'
 import { useEffect, useState } from 'react'
-import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet'
 import type { Library } from './api'
 import { getLibraries } from './api'
 import './App.css'
-import blueCircle from './circle_dark_blue.svg'
-import grayCircle from './circle_gray.svg'
-import greenCircle from './circle_green.svg'
-import redCircle from './circle_red.svg'
 import Info from './Info'
-import mapStyles from './Map.module.scss'
+import type { MarkerType } from './Map'
+import Map from './Map'
 import { getTimeRange, libraryOpen } from './time'
 import TimeControl from './TimeControl'
-
-const generalIcon = L.icon({
-  iconUrl: blueCircle,
-  iconSize: [20, 20],
-})
-
-const openIcon = L.icon({
-  iconUrl: greenCircle,
-  iconSize: [20, 20],
-})
-
-const selfServiceIcon = L.icon({
-  iconUrl: redCircle,
-  iconSize: [20, 20],
-})
-
-const unknownIcon = L.icon({
-  iconUrl: grayCircle,
-  iconSize: [20, 20],
-})
-
-const MapClick = ({ onClick }: { onClick?: (map: L.Map) => void }) => {
-  const map = useMapEvents({
-    click() {
-      console.log('clicked')
-      if (onClick) onClick(map)
-    },
-  })
-  return null
-}
 
 function App() {
   const [libraries, setLibraries] = useState<Library[]>([])
@@ -69,60 +33,31 @@ function App() {
   const selectedLibrary = libraries.find(l => l.Id === selectedLibraryId)
   const [from, to] = getTimeRange(libraries)
 
-  const markers = timeline ? (
-    <>
-      {libraries
+  const markers: MarkerType[] = timeline
+    ? libraries
+        .filter(library => library.Address.Coordinates)
         .map(library => [library, libraryOpen(library, selectedTime)] as const)
         .filter(([, status]) => status)
-        .filter(([library]) => library.Address.Coordinates)
-        .map(([library, open]) => {
-          return (
-            <Marker
-              key={library.Id}
-              position={library.Address.Coordinates as [number, number]}
-              icon={
-                open === 1
-                  ? openIcon
-                  : open === 2
-                  ? selfServiceIcon
-                  : unknownIcon
-              }
-              eventHandlers={{ click: () => setSelectedLibraryId(library.Id) }}
-            ></Marker>
-          )
-        })}
-    </>
-  ) : (
-    <>
-      {libraries
+        .map(([library, open]) => ({
+          id: library.Id,
+          coordinates: library.Address.Coordinates as [number, number],
+          type: open === 1 ? 'open' : open === 2 ? 'self-service' : 'unknown',
+        }))
+    : libraries
         .filter(library => library.Address.Coordinates)
-        .map(library => {
-          return (
-            <Marker
-              key={library.Id}
-              position={library.Address.Coordinates as [number, number]}
-              icon={generalIcon}
-              eventHandlers={{ click: () => setSelectedLibraryId(library.Id) }}
-            ></Marker>
-          )
-        })}
-    </>
-  )
+        .map(library => ({
+          id: library.Id,
+          coordinates: library.Address.Coordinates as [number, number],
+          type: 'general',
+        }))
 
   return (
     <>
-      <MapContainer
-        className={mapStyles.container}
-        center={[60.1674881, 24.9427473]}
-        zoom={11}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-        />
-        <MapClick onClick={() => setSelectedLibraryId('')} />
-        <MarkerClusterGroup maxClusterRadius={10}>{markers}</MarkerClusterGroup>
-      </MapContainer>
+      <Map
+        markers={markers}
+        onSelect={id => setSelectedLibraryId(id)}
+        onDeselect={() => setSelectedLibraryId('')}
+      />
       {selectedLibrary && <Info library={selectedLibrary} />}
       <TimeControl
         active={timeline}
---
+++ src/Map.tsx
@@ -0,0 +1,91 @@
+import MarkerClusterGroup from '@changey/react-leaflet-markercluster'
+import * as L from 'leaflet'
+import 'leaflet.markercluster/dist/MarkerCluster.css'
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet'
+import './App.css'
+import blueCircle from './circle_dark_blue.svg'
+import grayCircle from './circle_gray.svg'
+import greenCircle from './circle_green.svg'
+import redCircle from './circle_red.svg'
+import mapStyles from './Map.module.scss'
+const generalIcon = L.icon({
+  iconUrl: blueCircle,
+  iconSize: [20, 20],
+})
+
+const openIcon = L.icon({
+  iconUrl: greenCircle,
+  iconSize: [20, 20],
+})
+
+const selfServiceIcon = L.icon({
+  iconUrl: redCircle,
+  iconSize: [20, 20],
+})
+
+const unknownIcon = L.icon({
+  iconUrl: grayCircle,
+  iconSize: [20, 20],
+})
+
+export type MarkerType = {
+  id: string
+  coordinates: [number, number]
+  type: 'general' | 'open' | 'self-service' | 'unknown'
+}
+
+const MapClick = ({ onClick }: { onClick?: (map: L.Map) => void }) => {
+  const map = useMapEvents({
+    click() {
+      console.log('clicked')
+      if (onClick) onClick(map)
+    },
+  })
+  return null
+}
+
+const Map = ({
+  markers,
+  onSelect,
+  onDeselect,
+}: {
+  markers: MarkerType[]
+  onSelect: (id: string) => void
+  onDeselect: () => void
+}) => (
+  <MapContainer
+    className={mapStyles.container}
+    center={[60.1674881, 24.9427473]}
+    zoom={11}
+  >
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+    />
+    <MapClick onClick={() => onDeselect()} />
+    <MarkerClusterGroup maxClusterRadius={10}>
+      {markers.map(marker => {
+        const icon =
+          marker.type === 'open'
+            ? openIcon
+            : marker.type === 'self-service'
+            ? selfServiceIcon
+            : marker.type === 'unknown'
+            ? unknownIcon
+            : generalIcon
+        return (
+          <Marker
+            key={marker.id}
+            position={marker.coordinates}
+            icon={icon}
+            eventHandlers={{ click: () => onSelect(marker.id) }}
+          ></Marker>
+        )
+      })}
+    </MarkerClusterGroup>
+  </MapContainer>
+)
+
+export default Map
